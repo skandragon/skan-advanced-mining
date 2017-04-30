52c989555041fcbdffc66c9cc17bce0296921105
Michael Graff <explorer@flame.org>
2017-04-29 22:47:12 -0700

    localised names are an array

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -3,8 +3,8 @@ data:extend({
   {
     type = "technology",
     name = "skan-advanced-mining-1",
-    localised_name = "technology-name.skan-advanced-mining-1",
-    localised_description = "technology-description.skan-advanced-mining-1",
+    localised_name = {"technology-name.skan-advanced-mining-1"},
+    localised_description = {"technology-description.skan-advanced-mining-1"},
     icon = "__Advanced-Drills__/graphics/mining-drills/advanced-mining-drill.png",
     effects =
     {
@@ -29,8 +29,8 @@ data:extend({
   {
     type = "technology",
     name = "skan-advanced-mining-2",
-    localised_name = "technology-name.skan-advanced-mining-2",
-    localised_description = "technology-description.skan-advanced-mining-2",
+    localised_name = {"technology-name.skan-advanced-mining-2"},
+    localised_description = {"technology-description.skan-advanced-mining-2"},
     icon = "__Advanced-Drills__/graphics/mining-drills/elite-mining-drill.png",
     effects =
     {
@@ -57,8 +57,8 @@ data:extend({
   {
     type = "technology",
     name = "skan-advanced-mining-3",
-    localised_name = "technology-name.skan-advanced-mining-3",
-    localised_description = "technology-description.skan-advanced-mining-3",
+    localised_name = {"technology-name.skan-advanced-mining-3"},
+    localised_description = {"technology-description.skan-advanced-mining-3"},
     icon = "__Advanced-Drills__/graphics/mining-drills/ultimate-mining-drill.png",
     effects =
     {
